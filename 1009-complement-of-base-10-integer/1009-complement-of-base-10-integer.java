f677c6eddd67b765b09ad6e0abd95e6d59e67e35
Tanmay Agrawal <151424527+tanmayagrawal01@users.noreply.github.com>
2026-03-11 12:34:18 +0530

    Time: 6 ms (2.7%), Space: 42.2 MB (66.82%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        int i=0;
+        int ans=0;
+        if(n==0) return 1;
+        while(n>0){
+            int a=n&1;
+            n>>=1;
+            if(a==0) ans|=(1<<i);
+            // ans<<=1;
+            i++;
+            System.out.println(ans);
+        }
+        return ans;
+    }
+}
