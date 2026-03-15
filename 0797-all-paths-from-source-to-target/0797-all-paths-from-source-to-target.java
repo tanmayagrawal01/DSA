553c63586d596d1dbbddd9e1966e75622fe5c96f
Tanmay Agrawal <151424527+tanmayagrawal01@users.noreply.github.com>
2026-03-15 16:57:32 +0530

    Time: 2 ms (91.47%), Space: 48.2 MB (23.55%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,13 +1,12 @@
 class Solution {
     public void dfs(List<List<Integer>>res,List<Integer>l,int nod,boolean visited[],int graph[][]){
+        l.add(nod);
         if(nod==graph.length-1){
-            l.add(nod);
             res.add(new ArrayList<>(l));
             return;
         }
         if(visited[nod])return;
         visited[nod]=true;
-        l.add(nod);
         for(int n:graph[nod]){
             dfs(res,l,n,visited,graph);
             l.remove(l.size()-1);
