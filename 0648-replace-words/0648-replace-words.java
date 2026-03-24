cec165d6cf6dcaab404802eb60065b683d0e2090
Tanmay Agrawal <151424527+tanmayagrawal01@users.noreply.github.com>
2026-03-24 11:21:00 +0530

    Time: 23 ms (49.64%), Space: 56.5 MB (95.86%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        StringBuilder sb=new StringBuilder();
+        HashSet<String>set=new HashSet<>(dictionary);
+        String words[]=sentence.split(" ");
+        HashSet<Integer>lengths=new HashSet<>();
+        for(String i:dictionary){
+            int len=i.length();
+            lengths.add(len);
+        }
+        for(int i=0;i<words.length;i++){
+            for(int j:lengths){
+                if(words[i].length()<j) continue;
+                String str=words[i].substring(0,j);
+                if(set.contains(str)){
+                    words[i]=str;
+                }
+            }
+        }
+        return String.join(" ", words);
+    }
+}
