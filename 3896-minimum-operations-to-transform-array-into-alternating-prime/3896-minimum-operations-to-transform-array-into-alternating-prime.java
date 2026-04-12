5024616dbb74b94d506b288daf7f8cedbbe62ba6
Tanmay Agrawal <151424527+tanmayagrawal01@users.noreply.github.com>
2026-04-12 18:56:04 +0530

    Time: 1070 ms (0%), Space: 91.1 MB (0%) - LeetHub

---
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void primeno(boolean prime[]){
+        prime[0]=false;
+        prime[1]=false;
+        int n=prime.length;
+        for(int i=2;i*i<n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<n;j+=i){
+                    prime[j]=false;
+                }
+            }
+        }
+    }
+    public int minOperations(int[] nums) {
+        boolean prime[]=new boolean[1000001];
+        Arrays.fill(prime,true);
+        primeno(prime);
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            int x=nums[i];
+            if(i%2==0){
+                while(x<prime.length && !prime[x]){
+                    ans++;
+                    x++;
+                }
+            }else{
+                while(x<prime.length &&prime[x]){
+                    ans++;
+                    x++;
+                }
+            }
+        }
+        return ans;
+    }
+}
