13a519759ac905b0e3a01f8d29c62c810de45742
Tanmay Agrawal <151424527+tanmayagrawal01@users.noreply.github.com>
2026-05-17 16:37:32 +0530

    Time: 3 ms (21.12%), Space: 47.5 MB (61.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,21 +1,22 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Queue<int[]>q=new LinkedList<>();
-        int n=matrix.length,m=matrix[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    q.add(new int[]{i,j});
+        Set<Integer> rows = new HashSet();
+        Set<Integer> cols = new HashSet();
+        int n = matrix.length, m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
                 }
             }
         }
-        while(!q.isEmpty()){
-            int curr[]=q.poll();
-            for(int i=0;i<n;i++){
-                matrix[i][curr[1]]=0;
-            }
-            for(int i=0;i<m;i++){
-                matrix[curr[0]][i]=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
