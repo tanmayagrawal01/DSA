89fc2995519b2f598fc0a005ced67f3240b8e569
Tanmay Agrawal <151424527+tanmayagrawal01@users.noreply.github.com>
2026-06-21 21:24:12 +0530

    Time: 1248 ms (23.65%), Space: 47.2 MB (35.44%) - LeetHub

--- 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.java
+++ 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.java
@@ -1,47 +1,32 @@
 class Solution {
     public boolean first(long n, int x) {
-        n = Math.abs(n);
-
-        while (n >= 10) {
-            n /= 10;
-        }
-
-        return n == x;
+        int a = (int) (n / Math.pow(10, (int) Math.log10(n)));
+        return a == x;
     }
-
     public boolean last(long n, int x) {
-        return Math.abs(n % 10) == x;
+        return x == (n % 10);
     }
-
     public int countValidSubarrays(int[] nums, int x) {
         int n = nums.length;
-
         long[] prefix = new long[n];
         prefix[0] = nums[0];
-
         for (int i = 1; i < n; i++) {
             prefix[i] = prefix[i - 1] + nums[i];
         }
-
         int cnt = 0;
-
         for (int l = 0; l < n; l++) {
             for (int r = l; r < n; r++) {
-
                 long sum;
-
                 if (l == 0) {
                     sum = prefix[r];
                 } else {
                     sum = prefix[r] - prefix[l - 1];
                 }
-
                 if (first(sum, x) && last(sum, x)) {
                     cnt++;
                 }
             }
         }
-
         return cnt;
     }
 }
